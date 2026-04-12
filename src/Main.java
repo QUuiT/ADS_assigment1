09ce869b8a49b5256a6616104074fa4244db0f63
Ice <aisultan2007q.gmail.com git config --list fwfew>
2026-04-12 22:12:49 +0500

    Added Task 2 and Task 3

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,96 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        // Task 1
+
+        //Task 1
+//        Scanner input = new Scanner(System.in);
+//        int n = input.nextInt();
+//        ArrayList<ArrayList<Integer>> allLists = new ArrayList<>();
+//        while( n != 0){
+//            n--;
+//            int d = input.nextInt();
+//            ArrayList<Integer> numbers = new ArrayList<>();
+//            for (int i = 0 ; i < d ; i++){
+//                int number = input.nextInt();
+//                numbers.add(number);
+//            }
+//            allLists.add(numbers);
+//        }
+//        int q = input.nextInt();
+//        while (q != 0){
+//            q--;
+//            int x = input.nextInt();
+//            int y = input.nextInt();
+//            try {
+//                System.out.println(allLists.get(x - 1).get(y-1));
+//            } catch (IndexOutOfBoundsException e){
+//                System.out.println("ERROR!");
+//
+//            }
+//        }
+
+        //Task 2
+//        Scanner input = new Scanner(System.in);
+//        int n = input.nextInt();
+//        ArrayList<Integer> numbers = new ArrayList<>();
+//        for (int i = 0 ; i < n ; i++){
+//            int num = input.nextInt();
+//            numbers.add(num);
+//        }
+//        int q = input.nextInt();
+//
+//        while (q != 0){
+//            q--;
+//            String operation = input.next();
+//            if (operation.equals("Insert")){
+//                int index = input.nextInt();
+//                int number = input.nextInt();
+//                numbers.add(index,number);
+//            }
+//            else if (operation.equals("Delete")){
+//                int index = input.nextInt();
+//                numbers.remove(index);
+//            }
+//            else{
+//                System.out.println("Unknown command");
+//            }
+//        }
+//        for (int to : numbers){
+//            System.out.print(to + " ");
+//        }
+
+        //Task 3
+//        Scanner input = new Scanner(System.in);
+//        int n = input.nextInt();
+//        ArrayList<Integer> LeftEdge = new ArrayList<>();
+//        ArrayList<Integer> RightEdge = new ArrayList<>();
+//
+//        while(n != 0){
+//            n--;
+//            int operation = input.nextInt();
+//            switch(operation){
+//                case 1:
+//                    int numberLeft = input.nextInt();
+//                    LeftEdge.add(numberLeft);
+//                    break;
+//                case 2:
+//                    int numberRight = input.nextInt();
+//                    RightEdge.add(numberRight);
+//                    break;
+//                case 3:
+//                    System.out.println(LeftEdge.get(LeftEdge.size() - 1));
+//                    LeftEdge.remove(LeftEdge.size() - 1);
+//                    break;
+//                case 4:
+//                    System.out.println(RightEdge.get(RightEdge.size() - 1));
+//                    RightEdge.remove(RightEdge.size() - 1);
+//                    break;
+//            }
+//        }
+
+        //Task 4
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        ArrayList<ArrayList<Integer>> allLists = new ArrayList<>();
-        while( n != 0){
-            n--;
-            int d = input.nextInt();
-            ArrayList<Integer> numbers = new ArrayList<>();
-            for (int i = 0 ; i < d ; i++){
-                int number = input.nextInt();
-                numbers.add(number);
-            }
-            allLists.add(numbers);
-        }
-        int q = input.nextInt();
-        while (q != 0){
-            q--;
-            int x = input.nextInt();
-            int y = input.nextInt();
-            try {
-                System.out.println(allLists.get(x - 1).get(y-1));
-            } catch (IndexOutOfBoundsException e){
-                System.out.println("ERROR!");
-
-            }
-
-
-        }
+        
 
     }
-
 }
