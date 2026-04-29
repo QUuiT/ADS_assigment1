b0de9b00fa7a089f042bd7d411bdfef1922ec73a
Ice <aisultan2007q.gmail.com git config --list fwfew>
2026-04-29 19:21:27 +0500

    Tasks

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         //Task 1
-//
+
 //        String firstWord = input.nextLine();
 //        String secondWord = input.nextLine();
 //
